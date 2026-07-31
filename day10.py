d352218e8b24f3d3a07794026624b172e4571acd
developervivek01 <vivekvishwkarma108@gmail.com>
2026-07-31 18:28:49 +0530

    Delete day10.py

--- day10.py
+++
@@ -1,9 +0,0 @@
-'''DAY 10 OF 100DAYS OF
-PYTHON PROJECTS'''
-import random
-chars = "!@#$%^&*abcdefghi1234567890gh"
-
-password = ""
-for i in range(8):
-    password+=random.choice(chars)
-print("GENRATEDPASSWORD IS :",password)    
