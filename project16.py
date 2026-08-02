2d03ce99537aeeda5bab6c1294eab8624afa35de
vivekvishwkarma <vivekvishwkarma108@gmail.com>
2026-08-02 08:47:26 +0530

    Add files via upload

---
+++ project16.py
@@ -0,0 +1,10 @@
+"""DAY 16 OF 100DAYS 
+PYTHON PROJECTS"""
+username = "malwaremint"
+password = "12345"
+u = input("Enter your username :")
+p = input("Enter your password :")
+if u ==username and p == password:
+    print("LOGIN SUCCESFULLY!!!")
+else:
+    print("LOGIN FAILED!!!")    
