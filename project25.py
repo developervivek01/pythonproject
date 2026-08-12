34c5ec883cfb4c8f92c28a753448902cba03c86b
vivekvishwkarma <vivekvishwkarma108@gmail.com>
2026-08-12 10:01:13 +0530

    Add files via upload

---
+++ project25.py
@@ -0,0 +1,10 @@
+"""DAY 25 OF 100 DAYS 
+PYTHON PROJECT """
+number1 = int(input("Enter No 1 : "))
+number2 = int(input("Enter No 2 : "))
+number3 = int(input("Enter No 3 : "))
+
+i = 1
+while i <= 10:
+    print(i*number1,number2*i,number3*i)
+    i += 1
